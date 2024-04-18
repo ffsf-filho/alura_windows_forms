b16684b5bec6b58d41ead959de8b331bd3d18e8e
franciscof <franciscof@cetsp.com.br>
2024-04-18 10:44:09 -0300

    Implementa formulário Frm_ValidaCPF

---
+++ CursoWindowsForms/Frm_ValidaCPF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using CursoWindowsForms.Bibliotecas;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF : Form
+    {
+        public Frm_ValidaCPF()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            ValidaCPF validaCPF = new ValidaCPF();
+            bool cpfValido = validaCPF.Valida(Msk_CPF.Text);
+
+            if (cpfValido)
+            {
+                Lbl_Resultado.Text = "CPF VÁLIDO";
+                Lbl_Resultado.ForeColor = Color.Green;
+            }
+            else
+            {
+                Lbl_Resultado.Text = "CPF INVÁLIDO";
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+            
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+            Lbl_Resultado.Text = "";
+            Msk_CPF.Focus();
+        }
+    }
+}
