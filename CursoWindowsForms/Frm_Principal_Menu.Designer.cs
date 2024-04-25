719a1e7deb1077d40a6bef9952c5fd297119df3c
franciscof <franciscof@cetsp.com.br>
2024-04-25 09:55:03 -0300

    Inclui formulário Frm_Principal_Menu e inseri o menu

---
+++ CursoWindowsForms/Frm_Principal_Menu.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_Principal_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Principal_Menu));
+            this.Mnu_Principal = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.demosntraçãoKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helloWorldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mascaraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valídaCPFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valídaCPF2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valídaSenhaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Mnu_Principal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Mnu_Principal
+            // 
+            this.Mnu_Principal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.windowaToolStripMenuItem});
+            this.Mnu_Principal.Location = new System.Drawing.Point(0, 0);
+            this.Mnu_Principal.Name = "Mnu_Principal";
+            this.Mnu_Principal.Size = new System.Drawing.Size(800, 24);
+            this.Mnu_Principal.TabIndex = 0;
+            this.Mnu_Principal.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // windowaToolStripMenuItem
+            // 
+            this.windowaToolStripMenuItem.Name = "windowaToolStripMenuItem";
+            this.windowaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.windowaToolStripMenuItem.Text = "Windowa";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.demosntraçãoKeyToolStripMenuItem,
+            this.helloWorldToolStripMenuItem,
+            this.mascaraToolStripMenuItem,
+            this.valídaCPFToolStripMenuItem,
+            this.valídaCPF2ToolStripMenuItem,
+            this.valídaSenhaToolStripMenuItem});
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.novoToolStripMenuItem.Text = "Novo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // demosntraçãoKeyToolStripMenuItem
+            // 
+            this.demosntraçãoKeyToolStripMenuItem.Name = "demosntraçãoKeyToolStripMenuItem";
+            this.demosntraçãoKeyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.demosntraçãoKeyToolStripMenuItem.Text = "Demosntração Key";
+            // 
+            // helloWorldToolStripMenuItem
+            // 
+            this.helloWorldToolStripMenuItem.Name = "helloWorldToolStripMenuItem";
+            this.helloWorldToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.helloWorldToolStripMenuItem.Text = "Hello World";
+            // 
+            // mascaraToolStripMenuItem
+            // 
+            this.mascaraToolStripMenuItem.Name = "mascaraToolStripMenuItem";
+            this.mascaraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mascaraToolStripMenuItem.Text = "Mascara";
+            // 
+            // valídaCPFToolStripMenuItem
+            // 
+            this.valídaCPFToolStripMenuItem.Name = "valídaCPFToolStripMenuItem";
+            this.valídaCPFToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.valídaCPFToolStripMenuItem.Text = "Valída CPF";
+            // 
+            // valídaCPF2ToolStripMenuItem
+            // 
+            this.valídaCPF2ToolStripMenuItem.Name = "valídaCPF2ToolStripMenuItem";
+            this.valídaCPF2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.valídaCPF2ToolStripMenuItem.Text = "Valída CPF2";
+            // 
+            // valídaSenhaToolStripMenuItem
+            // 
+            this.valídaSenhaToolStripMenuItem.Name = "valídaSenhaToolStripMenuItem";
+            this.valídaSenhaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.valídaSenhaToolStripMenuItem.Text = "Valída Senha";
+            // 
+            // Frm_Principal_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Mnu_Principal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.Mnu_Principal;
+            this.Name = "Frm_Principal_Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
+            this.Mnu_Principal.ResumeLayout(false);
+            this.Mnu_Principal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Mnu_Principal;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem demosntraçãoKeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helloWorldToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mascaraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valídaCPFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valídaCPF2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valídaSenhaToolStripMenuItem;
+    }
+}
