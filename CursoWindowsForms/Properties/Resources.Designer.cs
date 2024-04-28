0d632b5ec0868755d6c8a28640b9168770f6360d
franciscof <franciscof@cetsp.com.br>
2024-04-28 12:41:03 -0300

    Anexa imagem key ao Resources ro projeto

--- CursoWindowsForms/Properties/Resources.Designer.cs
+++ CursoWindowsForms/Properties/Resources.Designer.cs
@@ -209,5 +209,15 @@ namespace CursoWindowsForms.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap key {
+            get {
+                object obj = ResourceManager.GetObject("key", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
