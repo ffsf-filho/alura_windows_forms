fb21268fe3a820c3b56e74db26129256103c1289
franciscof <franciscof@cetsp.com.br>
2024-05-03 09:01:15 -0300

    Implementa menu flutuante

--- CursoWindowsForms/Formularios_Curso_3/Frm_MenuFlutuante.cs
+++ CursoWindowsForms/Formularios_Curso_3/Frm_MenuFlutuante.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace CursoWindowsForms.Formularios_Curso_3
@@ -16,12 +17,30 @@ namespace CursoWindowsForms.Formularios_Curso_3
             {
                 //MessageBox.Show($"Cliquei com o botão {e.Button.ToString()}, na posição ({e.X},{e.Y})", "Botão Pressionado");
                 ContextMenuStrip ContextMenu = new ContextMenuStrip();
-                ToolStripMenuItem vTooTip001 = new ToolStripMenuItem();
-                vTooTip001.Text = "Item do menu 1";
 
-                ContextMenu.Items.Add(vTooTip001);
+                List<string> menu1 = new List<string>() { "Menu 1.1", "Menu 1.2" };
+                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 1",menu1));
+                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 2"));
+                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 3"));
+                
                 ContextMenu.Show(this,new Point(e.X, e.Y));
             }
+        }
+
+        private ToolStripMenuItem DesenhaItemMenu(string texto, List<string> listaDropDown = null)
+        {
+            ToolStripMenuItem vTooTip = new ToolStripMenuItem();
+            vTooTip.Text = texto;
+
+            if (listaDropDown != null)
+            {
+                foreach (string item in listaDropDown)
+                {
+                    vTooTip.DropDownItems.Add(item);
+                }
+            }
+
+            return vTooTip;
         }
     }
 }
