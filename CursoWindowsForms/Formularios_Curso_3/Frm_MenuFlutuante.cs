c4f030b9ba61461e3bc4e9a2358e2b219ba3bdbf
franciscof <franciscof@cetsp.com.br>
2024-05-03 08:14:22 -0300

    Implementa formulário Frm_MenuFlutuante

---
+++ CursoWindowsForms/Formularios_Curso_3/Frm_MenuFlutuante.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace CursoWindowsForms.Formularios_Curso_3
+{
+    public partial class Frm_MenuFlutuante : Form
+    {
+        public Frm_MenuFlutuante()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_MenuFlutuante_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show($"Cliquei com o botão {e.Button.ToString()}", "Botão Pressionado");
+            }
+        }
+    }
+}
--- CursoWindowsForms/Program.cs
+++ CursoWindowsForms/Program.cs
@@ -14,7 +14,7 @@ namespace CursoWindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_MouseCursor());
+            Application.Run(new Frm_MenuFlutuante());
         }
     }
 }
