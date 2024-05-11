1ffa9d8080b9e0ed03dd3eaf47f5efbc226d71f1
franciscof <franciscof@cetsp.com.br>
2024-05-11 12:45:10 -0300

    Adiciona evento Click ao menu flutuante do formulário Frm_MenuFlutuante

--- CursoWindowsForms/Formularios_Curso_3/Frm_MenuFlutuante.cs
+++ CursoWindowsForms/Formularios_Curso_3/Frm_MenuFlutuante.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -19,6 +20,7 @@ namespace CursoWindowsForms.Formularios_Curso_3
                 ContextMenuStrip ContextMenu = new ContextMenuStrip();
 
                 ItemMenu item1 = new ItemMenu("Item do menu 1", "Frm_DemonstracaoKey");
+
                 List<ItemMenu> menu1DropDown = new List<ItemMenu>
                 {
                     new ItemMenu("Mascara", "Frm_Mascara"),
@@ -28,13 +30,27 @@ namespace CursoWindowsForms.Formularios_Curso_3
                 ContextMenu.Items.Add(DesenhaItemMenu(item1,menu1DropDown));
                 
                 ItemMenu item2 = new ItemMenu("Item do menu 2");
-                ContextMenu.Items.Add(DesenhaItemMenu(item2));
+                ToolStripMenuItem vToolTipItem2 = DesenhaItemMenu(item2);
+                ContextMenu.Items.Add(vToolTipItem2);
 
                 ItemMenu item3 = new ItemMenu("Valida CPF", "Frm_ValidaCPF");
-                ContextMenu.Items.Add(DesenhaItemMenu(item3));
+                ToolStripMenuItem vToolTipItem3 = DesenhaItemMenu(item3);
+                ContextMenu.Items.Add(vToolTipItem3);
                 
                 ContextMenu.Show(this,new Point(e.X, e.Y));
+                vToolTipItem2.Click += new EventHandler(vToolTipItem2_Click);
+                vToolTipItem3.Click += new EventHandler(vToolTipItem3_Click);
             }
+        }
+
+        void vToolTipItem2_Click(object sender1, EventArgs e1)
+        {
+            MessageBox.Show("Selecionado o Item do menu 2");
+        }
+
+        void vToolTipItem3_Click(object sender1, EventArgs e1)
+        {
+            MessageBox.Show("Selecionado Valida CPF");
         }
 
         private ToolStripMenuItem DesenhaItemMenu(ItemMenu menu, List<ItemMenu> listaDropDown = null)
